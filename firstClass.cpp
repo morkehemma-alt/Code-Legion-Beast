5fbedcfe736d1648ba6e6f3f194e7bedb977bcf7
Emmanuel Morkeh <morkehemma@gmail.com>
2026-04-12 14:29:36 +0000

    loop project

---
+++ firstClass.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int age;
+    string name = "Cuppa";
+    cout<<"What is your age"<<endl;
+    cin>>age;
+    cout<<age;
+    cout<<name;
+
+
+
+    return 0;
+}
