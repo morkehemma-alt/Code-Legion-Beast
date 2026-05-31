39ef5e7f76bad44b247943b4017b9185d46ae772
Emmanuel Morkeh <morkehemma@gmail.com>
2026-05-31 11:43:13 +0000

    java Assignment

---
+++ variableinitialization-java/VariableInitialization.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.variableinitialization;
+
+/**
+ *
+ * @author LATITUDE
+ */
+public class VariableInitialization {
+    public static void main(String[] args){
+        int age = 20;
+        String name = "Emmanuel";
+        double price = 250.50;
+        boolean IsTrue = true;
+        
+    System.out.println(age);
+    System.out.println(name);
+    System.out.println(price);
+    System.out.println(IsTrue);
+          
+    
+    }
+    
+}
